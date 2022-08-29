61963d257d8aa5588e94daab7a4d72e2ed0ed9b3
Alejandro <73189430+Alecapra96@users.noreply.github.com>
2022-08-29 13:15:50 -0300

    Arreglando html

---
+++ source/js/toolsJS.js
@@ -0,0 +1,27 @@
+
+const $formsignature = document.getElementById("signature-event");
+
+$formsignature.addEventListener("submit", createSignature);
+
+function createSignature(event){
+    event.preventDefault();
+    let $nameSignature = document.getElementById("first-last-name").value.toUpperCase();
+    let $chargenameSignature = document.getElementById("charge-signature").value.toUpperCase();
+    let $divSignature = document.getElementById("sign-ready");
+    let $name = document.getElementById("name");
+    let $charge = document.getElementById("charge");
+    $name.textContent = $nameSignature;
+    $charge.textContent = $chargenameSignature;
+    $divSignature.style.visibility = "visible";
+}
+function CopyToClipboard() {
+let respText = document.getElementById("respText");
+    var copyBoxElement = document.getElementById('editor1');
+    copyBoxElement.contenteditable = true;
+    copyBoxElement.focus();
+    document.execCommand('selectAll');
+    document.execCommand("copy");
+    copyBoxElement.contenteditable = false;
+    respText.textContent = "Signature has been copied to the clipboard";
+    respText.style.color = "red";    
+}
