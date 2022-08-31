0733797dadb1f4ae27f0774b04143c7045626b2c
Alejandro <73189430+Alecapra96@users.noreply.github.com>
2022-08-31 12:46:44 -0300

    Creating tables

--- source/js/download.js
+++ source/js/download.js
@@ -1,13 +1,18 @@
+const homePageList =document.getElementById("homePage-list");
 
 const revitList = document.getElementById("revit-list");
 const autocadList = document.getElementById("autocad-list");
-
-const homePageList =document.getElementById("homePage-list");
+const navisworkList = document.getElementById("naviswork-list");
+const recapList = document.getElementById("recap-list");
+const bluebeamList = document.getElementById("bluebeam-list");
+const otherList = document.getElementById("other-list");
 
 const revitContainer = document.getElementById("revit-container");
 const autocadContainer = document.getElementById("autocad-container");
-
-
+const navisworkContainer = document.getElementById("naviswork-container");
+const recapContainer = document.getElementById("recap-container");
+const bluebeamContainer = document.getElementById("bluebeam-container");
+const otherContainer = document.getElementById("other-container");
 
 revitList.onclick = function(e){
     e.preventDefault();
@@ -19,14 +24,33 @@ autocadList.onclick = function(e){
     softwareShow(autocadList,autocadContainer);
     };
 
+navisworkList.onclick = function(e){
+    e.preventDefault();
+    softwareShow(navisworkList,navisworkContainer);
+    };
 
+recapList.onclick = function(e){
+    e.preventDefault();
+    softwareShow(recapList,recapContainer);
+    };
+
+bluebeamList.onclick = function(e){
+    e.preventDefault();
+    softwareShow(bluebeamList,bluebeamContainer);
+    };
+
+otherList.onclick = function(e){
+    e.preventDefault();
+    softwareShow(otherList,otherContainer);
+    };
+        
 
 function softwareShow(softwareList,softwareContainer){
     hideAllContainer();
-    console.log(softwareContainer)
     resetIsActive();
     softwareList.className = "is-active";
     softwareContainer.style.display = "block";
+    console.log(softwareContainer)
 }
 
 
@@ -36,9 +60,21 @@ function resetIsActive(){
     revitContainer.className = "";
     autocadList.className = "";
     autocadContainer.className= "";
-
+    navisworkList.className = "";
+    navisworkContainer.className = "";
+    recapList.className = "";
+    recapContainer.className = "";
+    bluebeamList.className = "";
+    bluebeamContainer.className = "";
+    otherList.className = "";
+    otherContainer.className = "";
 }
 function hideAllContainer(){
     revitContainer.style.display = "none";
     autocadContainer.style.display = "none";
+    navisworkContainer.style.display = "none";
+    recapContainer.style.display = "none";
+    bluebeamContainer.style.display = "none";
+    otherContainer.style.display = "none";
+
 }
